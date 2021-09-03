6397b45e3ab59178ede8d54c4598b81c3bf4077c
Vishnu Satheesh <vishnusatheeshpulickal555@gmail.com>
2021-09-03 22:10:07 +0530

    User section is created

--- models/user.js
+++ models/user.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
-    name:String,
-    email:String,
-    password:String,
-    street:String,
-    city:String,
-    district:String,
-    pincode:Number,
-    phone:Number,
+    name:{type:String,required:true},
+    email:{type:String,required:true,unique:true},
+    password:{type:String,required:true},
+    street:{type:String,default:''},
+    city:{type:String,required:true,default:''},
+    district:{type:String,required:true},
+    pincode:{type:Number,required:true},
+    phone:{type:Number,required:true},
     isAdmin:{type:Boolean,default:false}
 })
 
--- routers/user.js
+++ routers/user.js
@@ -1,9 +1,55 @@
 const express = require('express');
 const router = express.Router();
+const {User} = require('../models/user');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+require('dotenv/config');
 
-router.get('/',(req,res)=>{
-    res.send('response from user')
+// User Signup section
+router.post('/signup',async(req,res)=>{
+    const userEmail = await User.findOne({email: req.body.email});
+    if(userEmail) return res.send({success:false,message:'Sorry this email already in use'})
+    let user = await new User({
+        name:req.body.name,
+        email:req.body.email,
+        password:bcrypt.hashSync(req.body.password,10),
+        street:req.body.street,
+        city:req.body.city,
+        district:req.body.district,
+        pincode:req.body.pincode,
+        phone:req.body.phone,
+    })
+    user = await user.save();
+    if(!user) return res.status(400).send({success:false,message:'User cannot created'});
+    res.status(201).json({success:true,message:'User created successfully'});
 })
 
 
-module.exports = router;
+// User login section 
+router.post('/login',async(req,res)=>{
+   const user = await User.findOne({email:req.body.email});
+   if(!user) return res.status(400).json({success:false,message:'User is not found'})
+   if(user && bcrypt.compareSync(req.body.password,user.password)){
+       const token = jwt.sign({userEmail:user.email},process.env.JWT_SECRET,{expiresIn:'1d'})
+       res.status(200).send({success:true,token:token})
+   }else{
+       return res.status(400).json({success:false,message:'Email or password is incorrect'});
+   }
+})
+
+// List of all users
+router.get('/', async(req,res)=>{
+    let users = await User.find().select('-password');
+    if(!users) return res.status(400).json({success:false,message:'cannot get users'});
+    res.status(200).send(users)
+})
+
+//  user details
+router.get('/:id',async(req,res)=>{
+    let user = await User.findById(req.params.id);
+    if(!user) return res.status(400).json({success:false,message:'The user which given Id is not found'});
+    res.status(200).send(user)
+})
+
+
+module.exports = router;        
